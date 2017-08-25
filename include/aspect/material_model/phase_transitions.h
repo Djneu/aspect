4c4fa094e5f5b58fa19ed9cabba6d5946e1cdfd0
Derek Neuharth <neuh5852@vandals.uidaho.edu>
2017-08-25 12:28:58 -0700

    First pass as material model, only includes temperature dependent viscosity and temp/depth dependent density.

---
+++ include/aspect/material_model/phase_transitions.h
@@ -0,0 +1,68 @@
+/*
+  Copyright (C) 2013 - 2017 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _aspect_material_model_phase_transitions_h
+#define _aspect_material_model_phase_transitions_h
+
+#include <aspect/material_model/interface.h>
+#include <aspect/simulator_access.h>
+
+namespace aspect
+{
+	namespace MaterialModel
+	{
+		using namespace dealii;
+
+		/*Beginning of material model to test viscosity and density changes.
+		*/
+    template <int dim>
+    class PhaseTransitions : public MaterialModel::Interface<dim>, public ::aspect::SimulatorAccess<dim>
+		{
+			public:
+        virtual void evaluate(const MaterialModel::MaterialModelInputs<dim> &in,
+                              MaterialModel::MaterialModelOutputs<dim> &out) const;
+
+				virtual bool is_compressible () const;
+				virtual double reference_viscosity () const;
+				
+				static
+				void
+				declare_parameters (ParameterHandler &prm);
+
+				virtual
+				void
+				parse_parameters (ParameterHandler &prm);
+
+			private:
+				double reference_rho;
+				double reference_T;
+				double reference_compressibility;
+				double reference_specific_heat;
+				double thermal_alpha;
+				double eta;
+				double activation_energy;
+				double k_value;
+				double B;
+
+		};
+	}
+}
+
+#endif
---
+++ source/material_model/phase_transitions.cc
@@ -0,0 +1,146 @@
+/*
+  Copyright (C) 2011 - 2017 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <aspect/material_model/phase_transitions.h>
+#include <aspect/adiabatic_conditions/interface.h>
+#include <aspect/gravity_model/interface.h>
+
+namespace aspect
+{
+	namespace MaterialModel
+	{
+    template <int dim>
+    void
+    PhaseTransitions<dim>::
+    evaluate(const MaterialModel::MaterialModelInputs<dim> &in,
+             MaterialModel::MaterialModelOutputs<dim> &out) const
+		{
+			for (unsigned int i=0; i < in.position.size(); ++i)
+			{
+				const double temperature = in.temperature[i];
+				const double pressure = in.pressure[i];
+
+				//constant properties for now
+				out.compressibilities[i] = reference_compressibility;
+				out.specific_heat[i] = reference_specific_heat;
+				out.thermal_conductivities[i] = k_value;
+				out.thermal_expansion_coefficients[i] = thermal_alpha;
+
+				//placeholder viscosity equation with temperature dependence.
+				out.viscosities[i] = (B/2) * exp(activation_energy/(temperature*8.314));
+
+
+				//density equation with pressure and temperature dependence, likely will change when adiabatic conditions are introduced.
+				const double density_pressure_dependence = reference_rho * out.compressibilities[i] * (pressure - this->get_surface_pressure());
+				const double density_temperature_dependence = 1.0 - thermal_alpha * (temperature - reference_T);
+				out.densities[i]   = (reference_rho + density_pressure_dependence) * density_temperature_dependence;
+			}
+		}
+
+		template <int dim>
+		void
+		PhaseTransitions<dim>::declare_parameters (ParameterHandler &prm)
+		{
+			prm.enter_subsection("Material model");
+			{
+				prm.enter_subsection("Phase transitions");
+				{
+          prm.declare_entry ("Reference density", "3300",
+                             Patterns::Double (0),
+                             "Reference density $\\rho_0$. Units: $kg/m^3$.");
+          prm.declare_entry ("Reference temperature", "293",
+                             Patterns::Double (0),
+                             "The reference temperature $T_0$. The reference temperature is used "
+                             "in the density formula. Units: $K$.");
+          prm.declare_entry ("Viscosity", "5e24",
+                             Patterns::Double (0),
+                             "The value of the viscosity $\\eta$. Units: $kg/m/s$.");
+          prm.declare_entry ("Thermal conductivity", "4.7",
+                             Patterns::Double (0),
+                             "The value of the thermal conductivity $k$. "
+                             "Units: $W/m/K$.");
+          prm.declare_entry ("Reference specific heat", "1250",
+                             Patterns::Double (0),
+                             "The value of the specific heat $C_p$. "
+                             "Units: $J/kg/K$.");
+          prm.declare_entry ("Thermal expansion coefficient", "4e-5",
+                             Patterns::Double (0),
+                             "The value of the thermal expansion coefficient $\\beta$. "
+                             "Units: $1/K$.");
+          prm.declare_entry ("Compressibility", "5.124e-12",
+                             Patterns::Double (0),
+                             "The value of the compressibility $\\kappa$. "
+                             "Units: $1/Pa$.");
+					prm.declare_entry ("Viscosity constant", "9.1e11",
+														 Patterns::Double(0),
+														 "Preexponential constant for viscosity." "Units: None");
+					prm.declare_entry ("Activation energy", "300000",
+														 Patterns::Double(0),
+														 "Activation energy for viscosity equation." "Units: J/mol");
+
+        }
+        prm.leave_subsection();
+      }
+      prm.leave_subsection();
+    }
+
+		template <int dim>
+		void
+		PhaseTransitions<dim>::parse_parameters (ParameterHandler &prm)
+		{
+			prm.enter_subsection("Material model");
+			{
+				prm.enter_subsection("Phase transitions");
+				{
+				  reference_rho              = prm.get_double ("Reference density");
+          reference_T                = prm.get_double ("Reference temperature");
+					reference_compressibility  = prm.get_double ("Compressibility");
+          eta                        = prm.get_double ("Viscosity");
+          k_value                    = prm.get_double ("Thermal conductivity");
+          reference_specific_heat    = prm.get_double ("Reference specific heat");
+          thermal_alpha              = prm.get_double ("Thermal expansion coefficient");
+					B                          = prm.get_double ("Viscosity constant");
+					activation_energy          = prm.get_double ("Activation energy");
+        }
+        prm.leave_subsection();
+      }
+      prm.leave_subsection();
+
+      // Declare dependencies on solution variables
+      this->model_dependence.viscosity = NonlinearDependence::none;
+      this->model_dependence.density = NonlinearDependence::temperature;
+      this->model_dependence.compressibility = NonlinearDependence::none;
+      this->model_dependence.specific_heat = NonlinearDependence::none;
+      this->model_dependence.thermal_conductivity = NonlinearDependence::none;
+    }
+	}
+}
+
+namespace aspect
+{
+	namespace MaterialModel
+	{
+		ASPECT_REGISTER_MATERIAL_MODEL(PhaseTransitions,
+																	 "Phase transitions",
+																	 "Material model to eventually have phase transitions and melt.")
+	}
+}
+
