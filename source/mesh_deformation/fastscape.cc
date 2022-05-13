c84ea9764b3c55f9fe833ef169b74f8e65554643
EstherHeck <hecken@gfz-potsdam.de>
2022-05-13 09:44:32 +0200

    removed dublicate after rebase

--- source/mesh_deformation/fastscape.cc
+++ source/mesh_deformation/fastscape.cc
@@ -91,31 +91,6 @@ namespace aspect
         in.close();
       }
 
-      // Since we don't open these until we're on one processor, we need to check if the
-      // restart files exist before hand.
-      // TODO: This was quickly done and can likely be shortened/improved.
-      if (restart)
-        {
-          // Create variables for output directory and restart file
-          std::string dirname = this->get_output_directory();
-
-          std::ifstream in;
-          in.open(dirname + "fastscape_h_restart.txt");
-          if (in.fail())
-            AssertThrow(false,ExcMessage("Cannot open topography file to restart FastScape."));
-          in.close();
-
-          in.open(dirname + "fastscape_b_restart.txt");
-          if (in.fail())
-            AssertThrow(false,ExcMessage("Cannot open basement file to restart FastScape."));
-          in.close();
-
-          in.open(dirname + "fastscape_steps_restart.txt");
-          if (in.fail())
-            AssertThrow(false,ExcMessage("Cannot open steps file to restart FastScape."));
-          in.close();
-        }
-
       // second is for maximum coordinates, first for minimum.
       for (unsigned int i=0; i<dim; ++i)
         {
